0968ad9658697c030879badff23eb0d55e6d7942
apehex <apehex@protonmail.com>
2023-09-30 17:32:42 +0200

    Start with the utilities from the "batch tx" bot

---
+++ tests/test_logging.py
@@ -0,0 +1,23 @@
+import pytest
+
+from forta_toolkit.logging import setup_log_format
+
+# FORMAT ######################################################################
+
+def test_setup_format_always_returns_a_string():
+    __patterns = (
+        setup_log_format(),
+        setup_log_format(version='1.2'),
+        setup_log_format(pattern='test'),
+        setup_log_format(pattern='{version}'),
+        setup_log_format(pattern='test', version='0.1'))
+    assert all(type(__p) == str for __p in __patterns)
+
+def test_setup_format_includes_dash_only_when_necessary():
+    assert '-' not in setup_log_format(version='')
+    assert '-' in setup_log_format(version='0.6.1')
+
+# LOGGING #####################################################################
+
+def test_setup_logger_works_without_bot_metadata():
+    assert setup_logger() is None # no error
